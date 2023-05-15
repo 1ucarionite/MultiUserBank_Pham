804352ae62612a1017e775886d2f9ae4a8a3fd94
zaide <zaide@ZAIKO>
2023-05-15 16:19:46 -0700

    Add project files.

---
+++ MultiUserBank_Pham/Bank.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Lena Pham
+// IT112
+// Comments: tricky, but food to the brain. i think you should get a raise 
+// behaviors not implemented and why: none, i believe
+
+namespace MultiUserBank_Pham
+{
+    public class Bank
+    {
+        private const decimal InitialBalance = 10000;
+        private string[] users;
+        private string[] passwords;
+        private decimal[] balances;
+
+        public decimal BankBalance { get; }
+
+        public Bank()
+        {
+            users = new string[] { "jlennon", "pmccartney", "gharrison", "rstarr" };
+            passwords = new string[] { "johnny", "pauly", "georgy", "ringoy" };
+            balances = new decimal[] { 1250, 2500, 3000, 1000 };
+            BankBalance = InitialBalance;
+        }
+        
+        public bool VerifyCredentials(string username, string password)
+        {
+            int index = Array.IndexOf(users, username);
+            if (index >= 0 && passwords[index] == password)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public decimal GetBalance(string username)
+        {
+            int index = Array.IndexOf(users, username);
+            if (index >= 0)
+            {
+                return balances[index];
+            }
+            else
+            {
+                return InitialBalance;
+            }
+        }
+
+        public void UpdateBalance(string username, decimal newBalance)
+        {
+            int index = Array.IndexOf(users, username);
+            if (index >= 0)
+            {
+                balances[index] = newBalance;
+            }
+        }
+
+        public decimal Deposit(string username, decimal amount)
+        {
+            decimal currentBalance = GetBalance(username);
+            decimal newBalance = currentBalance + amount;
+            UpdateBalance(username, newBalance);
+            return newBalance;
+        }
+
+        public bool Withdraw(string username, decimal amount)
+        {
+            decimal currentBalance = GetBalance(username);
+            if (amount <= 500 && currentBalance >= amount)
+            {
+                decimal newBalance = currentBalance - amount;
+                UpdateBalance(username, newBalance);
+                return true;
+            }
+            else if (amount > 500 && currentBalance >= 500)
+            {
+                Console.WriteLine("Withdrawals are capped at $500. Withdrew $500.");
+                decimal newBalance = currentBalance - 500;
+                UpdateBalance(username, newBalance);
+                return true;
+            }
+            else if (amount > currentBalance)
+            {
+                Console.WriteLine("Withdrawal exceeds available balance.");
+                decimal newBalance = 0;
+                UpdateBalance(username, newBalance);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ MultiUserBank_Pham/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace MultiUserBank_Pham
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank bank = new Bank();
+            Console.WriteLine("Welcome to the Multi User Bank! Bank Balance: " + bank.BankBalance.ToString("C"));
+            Console.WriteLine("What would you like to do?");
+
+            while (true)
+            {
+                Console.WriteLine("1. Log In 2. Exit");
+
+                string input = Console.ReadLine();
+
+                if (input == "1")
+                {
+                    Console.Write("Enter your username: ");
+                    string username = Console.ReadLine();
+
+                    Console.Write("Enter your password: ");
+                    string password = Console.ReadLine();
+
+                    if (bank.VerifyCredentials(username, password))
+                    {
+                        decimal currentBalance = bank.GetBalance(username);
+
+                        Console.WriteLine("Welcome, " + username + "!");
+                        Console.WriteLine("Your current balance is: " + currentBalance.ToString("C"));
+
+                        while (true)
+                        {
+                            Console.WriteLine("1. Check Balance 2. Deposit 3. Withdraw 4. Log Out");
+
+                            input = Console.ReadLine();
+
+                            if (input == "1")
+                            {
+                                currentBalance = bank.GetBalance(username);
+                                Console.WriteLine("Your current balance is: " + currentBalance.ToString("C"));
+                            }
+                            if (input == "2")
+                            {
+                                Console.WriteLine("Please enter the amount to deposit:");
+                                decimal depositAmount = decimal.Parse(Console.ReadLine());
+
+                                decimal newBalance = bank.Deposit(username, depositAmount);
+                                Console.WriteLine("New Balance: " + newBalance.ToString("C"));
+                            }
+                            else if (input == "3")
+                            {
+                                Console.WriteLine("Please enter the amount to withdraw:");
+                                decimal withdrawAmount = decimal.Parse(Console.ReadLine());
+
+                                bool success = bank.Withdraw(username, withdrawAmount);
+
+                                if (success)
+                                {
+                                    decimal updatedBalance = bank.GetBalance(username);
+                                    Console.WriteLine("New Balance: " + updatedBalance.ToString("C"));
+                                }
+                            }
+                            else if (input == "4")
+                            {
+                                Console.WriteLine("Thank you for using our bank. Goodbye!");
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid credentials. Please try again.");
+                    }
+                }
+                if (input == "2")
+                {
+                    Console.WriteLine("Exiting...");
+                    break;
+                }
+
+            }
+
+        }
+
+    }
+
+
+
+
+
+}
